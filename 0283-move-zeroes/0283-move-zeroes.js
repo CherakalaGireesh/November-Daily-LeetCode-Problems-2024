9e1bb9605cbc3314fabf5318c1312eda708e044d
Cherakala Gireesh <135722119+cherakalagireesh@users.noreply.github.com>
2024-11-06 09:57:33 +0530

    Time: 3 ms (47.96%), Space: 55.8 MB (6.43%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.js
+++ 0283-move-zeroes/0283-move-zeroes.js
@@ -4,17 +4,24 @@
  */
 var moveZeroes = function(nums) {
     
-    let i = 0;
-
-    for(let num of nums)
-    {
-        if(num != 0)
-            nums[i++] = num;
-    }
-
-    for(let j = i; j < nums.length; j++)
-    {
-        nums[j] = 0;
-    }
+    let i = -1;
+    
+    for(let j = 0; j < nums.length; j++)
+        {
+            if(nums[j] == 0)
+            {
+                i = j;
+                break;
+            }
+        }
+    
+    for(let j = i + 1; j < nums.length; j++)
+        {
+            if(i != -1 && nums[j] != 0)
+            {
+                [nums[j], nums[i]] =[nums[i], nums[j]];
+                i++;
+            }
+        }
 
 };
